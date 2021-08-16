d296d125b9c4cf5115b2ff887583dfefd70bcb5b
Derek Torres <derek.n.torres@gmail.com>
2021-08-16 16:32:26 +0900

    init set up complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  has_many :bookings
+  has_many :socks
+  has_many :socks, through: :bookings
+  has_one_attached :photo
+
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :location, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
