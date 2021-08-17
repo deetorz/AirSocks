56628518d439a492013b1cef8339dd14394d34f5
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-17 16:54:57 +0900

    finished models and started dashboard

--- app/controllers/dashboard/bookings_controller.rb
+++ app/controllers/dashboard/bookings_controller.rb
@@ -1,4 +1,7 @@
 class Dashboard::BookingsController < ApplicationController
+
   def index
+    @bookings = policy_scope(Booking)
+    @bookings_as_owner = policy_scope([:dashboard, Booking])
   end
 end
