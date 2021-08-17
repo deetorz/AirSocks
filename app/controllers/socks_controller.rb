cf6542dcf5acb03fe3f92da24cae4c5578c7664c
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-08-17 14:55:37 +0900

    finished show sock

--- app/controllers/socks_controller.rb
+++ app/controllers/socks_controller.rb
@@ -1,5 +1,17 @@
 class SocksController < ApplicationController
+  before_action :set_sock, only: [:show, :edit, :update, :destroy]
   def index
-    @socks = Sock.all
+    # @socks = Sock.all
+    @socks = policy_scope(Sock)
+  end
+
+  def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_sock
+    @sock = Sock.find(params[:id])
+    authorize @sock
   end
 end
