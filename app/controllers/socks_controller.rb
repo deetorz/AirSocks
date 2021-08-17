bb998bc4838312c679b6f493ca88a39121ff8773
Derek Torres <derek.n.torres@gmail.com>
2021-08-17 15:02:30 +0900

    create a sock method done, plus view

--- app/controllers/socks_controller.rb
+++ app/controllers/socks_controller.rb
@@ -3,4 +3,26 @@ class SocksController < ApplicationController
     @socks = Sock.all
     @socks = policy_scope(Sock)
   end
+
+  def new
+    @sock = Sock.new
+    authorize @sock
+  end
+
+  def create
+    @sock = Sock.new(sock_params)
+    @sock.user = current_user
+    authorize @sock
+    if @sock.save
+      redirect_to @sock, notice: 'Sock added!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sock_params
+    params.require(:sock).permit(:name, :description, :color, :size, :price, :photo_url, :style)
+  end
 end
