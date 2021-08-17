e68f4782ff80c4c158d84cc2202efe6f311c9ff4
meljun picardal <meljun.one@gmail.com>
2021-08-17 22:08:36 +0900

    Set navbar links. Additionally, user can now see all the socks they own, their history, and the pending socks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    socks_path
+  end
+
   include Pundit
 
   # Pundit: white-list approach.
@@ -12,6 +17,7 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
 
   private
 
