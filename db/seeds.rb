5f6b4b01662d0b0925c70ed5d0fc9c74e0add7b8
meljun picardal <meljun.one@gmail.com>
2021-08-18 12:01:17 +0900

    dropped, created, migrated, seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ User.destroy_all
 Booking.destroy_all
 puts "Populating the database..."
 
-user = User.create!(username: "Derek", location: "Meguro", email: "derekkkkkkkkk@gmail.com", password: "123123")
+User.create!(username: "Derek", location: "Meguro", email: "derek@gmail.com", password: "123123")
 
 Sock.create(name: "cute purple socks", description: "Really cute purple socks that will make you feel sooo kawaii 💜", color: "purple", style: "high socks", size: 24.5, price: 2000, user_id: 1, photo_url: "https://m.media-amazon.com/images/I/61Xls0GWrRL._AC_UX679_.jpg")
 Sock.create(name: "Sexy thigh highs!", description: "These babies will bring all the boys to the yard", color: "magenta", style: "thigh highs", size: 20.0, price: 10000, user_id: 1, photo_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC2IvNRSNkC7FU7WhIJgYl7HipGZT-h7KFXA&usqp=CAU")
